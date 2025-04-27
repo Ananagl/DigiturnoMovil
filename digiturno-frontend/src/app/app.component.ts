7847961184095b41e07d256163c5857c7bb2890b
TiburoncinUjaja <gogeta1092@hotmail.com>
2025-04-26 22:13:08 -0500

    Componente app-banner-usco

--- digiturno-frontend/src/app/app.component.ts
+++ digiturno-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { BannerGlobalComponent } from './app/components/banner-global/banner-global.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, BannerGlobalComponent],
 })
 export class AppComponent {
   constructor() {}
---
+++ digiturno-frontend/src/app/app/components/banner-global/banner-global.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-global',
+  templateUrl: './banner-global.component.html',
+  styleUrls: ['./banner-global.component.scss'],
+})
+export class BannerGlobalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
