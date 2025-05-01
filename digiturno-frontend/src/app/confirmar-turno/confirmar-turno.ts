461a244f26e2ce6e1012ed6c60db25c80de3a7de
javier <javier00parra@gmail.com>
2025-05-01 13:06:50 -0500

    Subo version estetica y con arreglo de datos

--- digiturno-frontend/src/app/confirmar-turno/confirmar-turno.ts
+++ digiturno-frontend/src/app/confirmar-turno/confirmar-turno.ts
@@ -4,6 +4,7 @@ import { IonicModule } from '@ionic/angular';
 import { BannerGlobalComponent } from '../components/banner-global/banner-global.component';
 import { TurnoStateService, TurnoData } from '../services/turno.service';
 import { TurnoApiService } from '../services/turno-api.service';
+import { TipoTurno, SubTipoTurno, TIPOS_TURNO, SUBTIPOS_TURNO } from '../interfaces/turno.interface';
 
 @Component({
   selector: 'app-confirmar-turno',
@@ -14,6 +15,8 @@ import { TurnoApiService } from '../services/turno-api.service';
 })
 export class ConfirmarTurnoComponent implements OnInit {
   datos!: TurnoData;
+  tipoNombre = '';
+  subtipoNombre = '';
   resultado: { id: number; message: string } | null = null;
   cargando = false;
   error: string | null = null;
@@ -25,6 +28,11 @@ export class ConfirmarTurnoComponent implements OnInit {
 
   ngOnInit() {
     this.datos = this.turnoState.getTurnoData();
+    // Mapear IDs a nombres
+    const tipo = TIPOS_TURNO.find(t => t.id === this.datos.tipo_turno_id);
+    this.tipoNombre = tipo ? tipo.nombre : 'N/A';
+    const subtipo = SUBTIPOS_TURNO.find(s => s.id === this.datos.subtipo_turno_id!);
+    this.subtipoNombre = subtipo ? subtipo.nombre : '-';
   }
 
   confirmar() {
@@ -32,15 +40,14 @@ export class ConfirmarTurnoComponent implements OnInit {
     this.error = null;
 
     this.turnoApi.crear(this.datos).subscribe(
-      (res) => {
+      res => {
         this.resultado = res;
         this.cargando = false;
       },
-      (err) => {
+      err => {
         this.error = err.error?.error || err.message || 'Error al guardar turno';
         this.cargando = false;
       }
     );
   }
 }
-
